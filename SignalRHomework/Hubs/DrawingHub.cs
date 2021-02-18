45c8efa22439a08fcd12f761f94109a898dc2156
baroniunas91 <61449435+baroniunas91@users.noreply.github.com>
2021-02-18 19:34:55 +0200

    Add project files.

---
+++ SignalRHomework/Hubs/DrawingHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRHomework.Hubs
+{
+    public class DrawingHub : Hub
+    {
+        public async Task SendCoord(int x, int y)
+        {
+            await Clients.Others.SendAsync("receiveCoord", x, y);
+        }
+    }
+}
